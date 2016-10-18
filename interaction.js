84d7e3916821cb569ba08ad36af105b4b15666d4
Alex <indifferentalex@outlook.com>
2016-10-19 00:08:25 +0200

    Testing logic and correct or wrong answer checking

--- interaction.js
+++ interaction.js
@@ -1,9 +1,48 @@
 jQuery(function() {
+	var options = [{ key: "articoloDeterminativo", suggestion: "Articolo Determinativo" },
+								 { key: "articoloIndeterminativo", suggestion: "Articolo Indeterminativo" },
+								 { key: "preposizioneDi", suggestion: "Preposizione Di" }];
+	var modes = ["allenamento", "verifica"];
+
+	var words = [{ parola: "gatto", articoloDeterminativo: "il", articoloIndeterminativo: "un", preposizioneDi: "del" },
+					 		 { parola: "albero", articoloDeterminativo: "l'", articoloIndeterminativo: "un", preposizioneDi: "dell'" }];
+
+	function getRandomInt(min, max) {
+	  return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	var option;
+	var word;
+
+	function loadWord() {
+		option = options[getRandomInt(0, options.length - 1)];
+		word = words[getRandomInt(0, words.length - 1)];
+
+		$("#answer").html("");
+		$("#option-category").html(option["suggestion"]);
+		$("#question").html(word["parola"]);
+	}
+
+	function checkAnswer(submitted) {
+		console.log(submitted);
+		console.log(word[option["key"]]);
+
+		if (word[option["key"]] == submitted) {
+			console.log("Correct");
+		} else {
+			console.log("Wrong");
+		}
+	}
+
+	loadWord();
+
 	$("#answer").keypress(function(e) {
 		if (e.which == 13) {
 			e.preventDefault();
 
-			alert($("#answer").html());
+			checkAnswer($("#answer").html());
+
+			loadWord();
 		}
 	});
 });
